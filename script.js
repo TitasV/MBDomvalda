19e616bfe608547d4c943f7542b94ea0b1b78aaa
TitasV <78471645+TitasV@users.noreply.github.com>
2025-02-15 21:30:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+function changeBackground(imageUrl) {
+    if (window.innerWidth <= 800) return;
+
+    const header = document.querySelector("header");
+    header.style.backgroundImage = `url('${imageUrl}')`;
+}
+
+document.getElementById("first-div").addEventListener("mouseover", () => {
+    changeBackground("images/phone-call.jpg");
+});
+
+document.getElementById("second-div").addEventListener("mouseover", () => {
+    changeBackground("images/examination.jpg");
+});
+
+document.getElementById("third-div").addEventListener("mouseover", () => {
+    changeBackground("images/calculating-cost.jpg");
+});
+
+document.getElementById("fourth-div").addEventListener("mouseover", () => {
+    changeBackground("images/repair.jpg");
+});
